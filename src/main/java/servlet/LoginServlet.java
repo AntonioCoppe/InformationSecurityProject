b6389d029a595dd0d228fc080ac3d4a3575460ab
Coppe Antonio (Student Com19) <antonio.coppe@stud-inf.unibz.it>
2022-05-30 10:51:51 +0200

    Fixed Login SQL vulnerability

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -4,9 +4,9 @@ import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 import jakarta.servlet.ServletException;
@@ -20,84 +20,97 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	private static final String USER = "sa";
 	private static final String PWD = "Riva96_shared_db";
 	private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 	private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=examDB;encrypt=true;trustServerCertificate=true;";
-    
+
 	private static Connection conn;
-	
+
 	/**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-    }
-    
-    public void init() throws ServletException {
-    	try {
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		try {
 			Class.forName(DRIVER_CLASS);
-			
-		    Properties connectionProps = new Properties();
-		    connectionProps.put("user", USER);
-		    connectionProps.put("password", PWD);
-	
-	        conn = DriverManager.getConnection(DB_URL, connectionProps);
-		    
-		    System.out.println("User \"" + USER + "\" logged into the database.");
-    	
-    	} catch (ClassNotFoundException | SQLException e) {
+
+			Properties connectionProps = new Properties();
+			connectionProps.put("user", USER);
+			connectionProps.put("password", PWD);
+
+			conn = DriverManager.getConnection(DB_URL, connectionProps);
+
+			System.out.println("User \"" + USER + "\" logged into the database.");
+
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
-		
-		
+
 		/*
-		 * Validate User input before accepting it and should solve SQL Injection as well AS XSS Reflected, for stored 
-		 * XSS the input needs to be validated from the SendMailServlet class.
+		 * Here is available the sql Injeciton ""free""" code
 		 */
-		
+
 		String email = request.getParameter("email");
 		String pwd = request.getParameter("password");
-		
-		
-		/*
-		 * DO NOT USE conn.createStatement()
-		 * 
-		 * Better to prepare a query before with something like: 
-		 * 				prepareStatment stmt = connection.prepareStatment(query); 
-		 * 				String query = "SELECT * FROM [user] WHERE email = ?" + "AND password = ?"
-		 */
-		
-		try (Statement st = conn.createStatement()) {
-			ResultSet sqlRes = st.executeQuery(
-				"SELECT * "
-				+ "FROM [user] "
-				+ "WHERE email='" + email + "' "
-					+ "AND password='" + pwd + "'"
-			);
-			
+
+		String query = "SELECT * FROM [user] WHERE email = ? AND password = ?";
+
+		try {
+
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setString(1, email);
+			statement.setString(2, pwd);
+			ResultSet sqlRes = statement.executeQuery();
+
 			if (sqlRes.next()) {
 				request.setAttribute("email", sqlRes.getString(3));
 				request.setAttribute("password", sqlRes.getString(4));
-				
+
 				System.out.println("Login succeeded!");
 				request.setAttribute("content", "");
 				request.getRequestDispatcher("home.jsp").forward(request, response);
-				
-				
+
 			} else {
 				System.out.println("Login failed!");
 				request.getRequestDispatcher("login.html").forward(request, response);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			request.getRequestDispatcher("login.html").forward(request, response);
 		}
+		
+		/*
+		 * Here is available the Vulnerable code
+		 */
+
+		/*
+		 * try (Statement st = conn.createStatement()) { ResultSet sqlRes =
+		 * st.executeQuery( "SELECT * " + "FROM [user] " + "WHERE email='" + email +
+		 * "' " + "AND password='" + pwd + "'" );
+		 * 
+		 * if (sqlRes.next()) { request.setAttribute("email", sqlRes.getString(3));
+		 * request.setAttribute("password", sqlRes.getString(4));
+		 * 
+		 * System.out.println("Login succeeded!"); request.setAttribute("content", "");
+		 * request.getRequestDispatcher("home.jsp").forward(request, response);
+		 * 
+		 * 
+		 * } else { System.out.println("Login failed!");
+		 * request.getRequestDispatcher("login.html").forward(request, response); }
+		 * 
+		 * } catch (SQLException e) { e.printStackTrace();
+		 * request.getRequestDispatcher("login.html").forward(request, response); }
+		 */
 	}
 }
--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -4,9 +4,9 @@ import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 import jakarta.servlet.ServletException;
@@ -20,75 +20,120 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	private static final String USER = "sa";
 	private static final String PWD = "Riva96_shared_db";
 	private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 	private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=examDB;encrypt=true;trustServerCertificate=true;";
-    
+
 	private static Connection conn;
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterServlet() {
-        super();
-    }
-    
-    public void init() throws ServletException {
-    	try {
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegisterServlet() {
+		super();
+	}
+
+	public void init() throws ServletException {
+		try {
 			Class.forName(DRIVER_CLASS);
-			
-		    Properties connectionProps = new Properties();
-		    connectionProps.put("user", USER);
-		    connectionProps.put("password", PWD);
-	
-	        conn = DriverManager.getConnection(DB_URL, connectionProps);
-		    
-		    //System.out.println("User \"" + USER + "\" registered to the database.");
-    	
-    	} catch (ClassNotFoundException | SQLException e) {
+
+			Properties connectionProps = new Properties();
+			connectionProps.put("user", USER);
+			connectionProps.put("password", PWD);
+
+			conn = DriverManager.getConnection(DB_URL, connectionProps);
+
+			System.out.println("User \"" + USER + "\" registered to the database.");
+
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
-		
-		// The replacement escapes apostrophe special character in order to store it in SQL
+
+		// The replacement escapes apostrophe special character in order to store it in
+		// SQL
 		String name = request.getParameter("name").replace("'", "''");
-		String surname = request.getParameter("surname").replace("'", "''");;
-		String email = request.getParameter("email").replace("'", "''");;
-		String pwd = request.getParameter("password").replace("'", "''");;
-		
-		try (Statement st = conn.createStatement()) {
-			ResultSet sqlRes = st.executeQuery(
-				"SELECT * "
-				+ "FROM [user] "
-				+ "WHERE email='" + email + "'"
-			);
-			
+		String surname = request.getParameter("surname").replace("'", "''");
+		;
+		String email = request.getParameter("email").replace("'", "''");
+		;
+		String pwd = request.getParameter("password").replace("'", "''");
+		;
+
+		String SelectQuery = "SELECT * FROM [user] WHERE email = ?";
+
+		/*
+		 * Here is available the sql Injeciton ""free""" code
+		 */
+
+		try {
+
+			PreparedStatement statement = conn.prepareStatement(SelectQuery);
+			statement.setString(1, email);
+			ResultSet sqlRes = statement.executeQuery();
+
 			if (sqlRes.next()) {
 				System.out.println("Email already registered!");
 				request.getRequestDispatcher("register.html").forward(request, response);
-				
+
 			} else {
-				st.execute(
-					"INSERT INTO [user] ( name, surname, email, password ) "
-					+ "VALUES ( '" + name + "', '" + surname + "', '" + email + "', '" + pwd + "' )"
-				);
-				
+
+				String InsertQuery = "SET NOCOUNT ON INSERT INTO [user] (name, surname, email, password ) VALUES (?, ?, ?, ?)";
+
+				PreparedStatement Insertstatement = conn.prepareStatement(InsertQuery);
+				Insertstatement.setString(1, name);
+				Insertstatement.setString(2, surname);
+				Insertstatement.setString(3, email);
+				Insertstatement.setString(4, pwd);
+
+				ResultSet sqlInsRes = Insertstatement.executeQuery();
+
+				sqlInsRes.next();
+
 				request.setAttribute("email", email);
 				request.setAttribute("password", pwd);
-				
+
 				System.out.println("Registration succeeded!");
 				request.getRequestDispatcher("home.jsp").forward(request, response);
+
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			request.getRequestDispatcher("register.html").forward(request, response);
 		}
+
+		/*
+		 * Here is available the vulnerable code to SQL injection
+		 */
+
+		/*
+		 * 
+		 * try (Statement st = conn.createStatement()) { ResultSet sqlRes =
+		 * st.executeQuery( "SELECT * " + "FROM [user] " + "WHERE email='" + email + "'"
+		 * );
+		 * 
+		 * if (sqlRes.next()) { System.out.println("Email already registered!");
+		 * request.getRequestDispatcher("register.html").forward(request, response);
+		 * 
+		 * } else { st.execute( "INSERT INTO [user] ( name, surname, email, password ) "
+		 * + "VALUES ( '" + name + "', '" + surname + "', '" + email + "', '" + pwd +
+		 * "' )" );
+		 * 
+		 * request.setAttribute("email", email); request.setAttribute("password", pwd);
+		 * 
+		 * System.out.println("Registration succeeded!");
+		 * request.getRequestDispatcher("home.jsp").forward(request, response); }
+		 * 
+		 * } catch (SQLException e) { e.printStackTrace();
+		 * request.getRequestDispatcher("register.html").forward(request, response); }
+		 */
 	}
 
 }
